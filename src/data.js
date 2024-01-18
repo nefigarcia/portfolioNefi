0cae101e10f6611ae73ba18a3a418575bcd5b68f
Nefi Garcia Gutierrez <bluedivee@hotmail.com>
2024-01-18 13:41:13 -0600

    portfolio2024 (#1)
    Co-authored-by: Nefi Garcia Gutierrez <nefigarcia@ibm.com>

--- src/data.js
+++ src/data.js
@@ -5,8 +5,8 @@ export const placeInfo=[
         headerSubtitle:'React, Css, SE6',
         description:'Used top new technologies as REact framework, ECMAScript 6 with webGl and Three library',
         works:'Jobs done',
-        example1:'www.organizacionx.com',
-        example2:'www.rosystems.toltechconsulting.com',
+        example1:'https://clincoln.tech/',
+        example2:'https://g3cars.clincoln.tech/',
         example3:'This portfolio was builded with React ',
         lexample1:'https://www.organizacionx.com',
         lexample2:'https://www.rosystems.toltechconsulting.com'
@@ -14,17 +14,17 @@ export const placeInfo=[
     {
         id:2,
         headerTitle:'Backend',
-        headerSubtitle:'SQL, Spring Boot framework, Data migration',
-        description:'Large systems built using different components with Spring Boot , sql, json, ajax, java',
+        headerSubtitle:'SQL, Spring Boot framework, Data migration, Node, Heroku',
+        description:'Large systems built using different components with Spring Boot , sql, json, ajax, java, Node, Some of the backend repositories built using Node REST are private for security reasons',
         works:'Projects',
-        example1:'www.toltechconsulting.com',
+        example1:'https://github.com/nefigarcia',
         lexample1:'https://www.toltechconsulting.com'
 
     },
     {
         id:3,
         headerTitle:'Data',
-        headerSubtitle:'HP AML, SQL,Access, Excel',
+        headerSubtitle:'HP AML, SQL,Access, Excel, MongoDB',
         description:'Experience with Data migration to Big data using HP ALM tool. Data analyst, macros design visul basic, accees',
         works:'Projects',
         example1:'ask me to show you some examples'
