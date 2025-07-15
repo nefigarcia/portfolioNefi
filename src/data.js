5d0505e4a884b31388688d5f3cc01d2233db8fce
nefigarcia <bluedivee@hotmail.com>
2025-07-14 18:47:00 -0600

    for AI Health app

--- src/data.js
+++ src/data.js
@@ -24,13 +24,18 @@ export const placeInfo=[
     {
         id:3,
         headerTitle:'Data',
-        headerSubtitle:'HP AML, SQL,Access, Excel, tableau, Kafka, Airflow, Python, Java, Tableau, SSIS, GCP(Bigquery), Snowflake, Spark, AWS(S3), AI tools Copilot, Gemini Firebase',
+        headerSubtitle:'AI Agents, AI tools, HP AML, SQL,Access, Excel, tableau, Kafka, Airflow, Python, Java, Tableau, SSIS, GCP(Bigquery), Snowflake, Spark, AWS(S3), AI tools Copilot, Gemini Firebase',
         description:'ETL Development | Automated Data Pipelines | Data Visualization | Scalable Code Solutions',
         works:'Projects',
         example1:'(Click) Jobs Analysis, pipeline automatation for ETL process',
         lexample1:'https://datapipelinejobs.rosystems.net/',
         example2:'(Click) Github repository',
-        lexample2:'https://github.com/nefigarcia/outsourcing-analysis'}
+        lexample2:'https://github.com/nefigarcia/outsourcing-analysis',
+        example3:'(Click) HealthAI with AI Agent and AI tools',
+        lexample3:'https://healthai.rosystems.net',
+        example4:'(Click) Github repository for HealthAI',
+        lexample4:'https://github.com/nefigarcia/HealthAI-assistant.git'
+    }
 ];
 export const detailInfo=
 {
